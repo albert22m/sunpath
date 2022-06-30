94ca145a1183f43d406c8a985024f1d64e16b143
albert22m <108478277+albert22m@users.noreply.github.com>
2022-06-30 17:28:37 +0200

    Update solarpath.m

--- solarpath.m
+++ solarpath.m
@@ -4,8 +4,8 @@ interval = 1e6;
 xt = linspace(0, 2*pi*365.15, interval);
 xd = linspace(0, 2*pi, interval);
 
-axis = 23.435; %23.435
-lambda = 41.433; %41.433
+axis = 23.435;
+lambda = 41.433;
 delta = (deg2rad(axis))*sin(xd-pi/2);
 
 y = deg2rad(90-lambda)*sin(xt-pi/2);
